cec48513551b30ae1a86bcd0aeb5e2cf04b4c4a4
TheophileBonifaceAchille <theophile.boniface@gmail.com>
2020-10-27 14:14:36 +0100

    README and Tribunal function

--- Citizen.sol
+++ Citizen.sol
@@ -3,19 +3,38 @@ pragma solidity >=0.6.0;
 import "./CitizenErc20.sol";
 
 contract Citizen {
+    address payable etat;
+
     CitizenErc20 public token;
 
-    address payable etat;
+    address private _sage1;
+    address private _sage2;
+    address private _sage3;
+
+    constructor(
+        address payable _addr,
+        address payable sage1,
+        address payable sage2,
+        address payable sage3
+    ) public {
+        etat = _addr;
+        _sage1 = sage1;
+        _sage2 = sage2;
+        _sage3 = sage3;
+    }
 
     /// @dev struct Citoyen
     struct Citoyen {
         bool isSage; // false if the member is not an sage, true is an sage
+        uint256 voteJudge; // if 3 sage approve, isJudge = true
+        bool isJudge; // false if the member is not an judge, true is an judge
         bool workeur; // false if the workeur is not an sage, true is an workeur
         bool jobless; // false if the jobless is not an sage, true is an jobless
-        bool banned; //
+        bool banned; // banned with timestamp
         uint256 age; // for pension retreat
         uint256 idEnterprise; // for workeur true, and jobless false and inverse
-        uint256 wallet; // wallet Citoyen
+        int256 wallet; // wallet Citoyen
+        mapping(address => bool) didVote; // mapping to check that an address can not vote twice
     }
 
     /// @dev struct Proposal
@@ -34,45 +53,64 @@ contract Citizen {
         uint256 deces;
     }
 
-    // mapping
-
     /// @dev mapping from an address to a Member
     mapping(address => Citoyen) public citoyen;
 
-    /// @dev mapping from an address to a Member
+    /// @dev mapping from an address to a Compagnie
     mapping(address => Compagnie) public compagnie;
 
-    // Modifier
+    /// @dev mapping from an address to a Taxe
+    mapping(address => Taxe) public taxe;
 
+    /// @dev modifier for Sage function
     modifier onlySage() {
         require(
             citoyen[msg.sender].isSage == true,
-            "only sage can call this function"
+            "only Judge can call this function"
         );
         _;
     }
 
-    // Fonction
+    /// @dev modifier for Judge function
+    modifier onlyJudge() {
+        require(
+            citoyen[msg.sender].isJudge == true,
+            "only Judge can call this function"
+        );
+        _;
+    }
 
-    // 100 citizen attribué par citoyen crée
+    /// @dev add citoyen
     function addCitoyen(address _addr) public onlySage {
+        // 100 citizen attribué par citoyen crée
         // Sage aprouve un citoyen dans le pay
     }
 
-    // Rajouter un vote pour que les citoyens elisent un sage ou le blacklist
-    // setSage met en depot 100 citizen
-    // Election dur 1 semaine
-
+    /// @dev Sage elected
     function setSage(address _addr) public onlySage {
         citoyen[_addr].isSage = true;
     }
 
+    /// @dev Sage kicked
     function unsetSage(address _addr) public onlySage {
         citoyen[_addr].isSage = false;
     }
 
-    // Rajouter un vote pour que les sages approuvent une entreprise et la créent
+    /// @dev enum pain
+    enum Peines {legere, lourde, grave, crime}
 
-    // Dans cet état les peines consistent à se faire retirer du CITIZEN. 4 types de peines: légère -5 citizen,
-    // lourde -50, grave -100, crime contre la nation 10 ans de ban et citizen 0.
+    /// @dev court of pain
+    function peinesCitizen(address _addr, Peines _peines) public onlyJudge {
+        (_addr != etat, "etat cannot be judgement !");
+        if (_peines == Peines.legere) {
+            citoyen[_addr].wallet -= 5 * 10**18;
+        } else if (_peines == Peines.lourde) {
+            citoyen[_addr].wallet -= 10 * 10**18;
+        } else if (_peines == Peines.grave) {
+            citoyen[_addr].wallet -= 100 * 10**18;
+        } else if (_peines == Peines.crime) {
+            citoyen[_addr].banned = true;
+            citoyen[_addr].wallet = 0;
+        } else revert("Invalid vote");
+    }
 }
---
+++ README.md
@@ -0,0 +1,42 @@
+# Citizen Project
+
+## **Deploiment**
+
+L'adress de déploiment sera l'Etat (le super admin).
+L'Etat pourra choisir ses 3 premiers sages.
+Ajouter l'adresse du token déploiyé ici: `0x7602c2B3e37b0dd5E04E44dD926f257701c962d8`
+
+_Tokken = CITIZEN_
+
+_Symbol = CTZ_
+
+_Etat =_ **_1 000 000 000 CTZ_**
+
+_CAP =_ **_1 000 000 000 CTZ_**
+
+## **Etat Sage Juge**
+
+L'Etat et les sages peuvent ajouter des citoyens avec un wallet de 100 CITIZEN.
+
+L'etat et les 3 sages peuvent élire un autre sages si 3 votes son contabilisez, dans une mimite de dix sages max. Un sage peut-être exclu si 5 vote sont contabilisez.
+Lorqu'un sage est élus, il doit mettre en dépôt 100 CTZ
+
+Les sages peuvent elires 3 juges qui pourront donner des peines/amandes aux citoyens.
+
+## **Citoyens**
+
+Les citoyens peuvent travailler pour les entreprisent du pays pour y gagner un salaire, sur ce salaire :
+
+- 10% sont envoyés dans une caisse COMMUNE à tous les citoyens. A tout moment les citoyens peuvent la consulter, cette caisse peut être utilisé que par l'état ou les sages.
+- 10% dans une caisse chômage, si le citoyen perd son travaille, il fait la demande auprès des sages qui lui addriburont ses droits.
+- 10% dans une caisse maladie, il ne pourront en bénéficié que si son statut est "malade". Il devra être validé par un sage.
+- 10% dans un caisse retraite, il pourrat retirer ca cotisation qu'après 60ans.
+
+## **Entreprise**
+
+Une entreprise doit s'enregistrer auprès de l'Etat et déposer 100 000 CTZ pour payer les salaires des citoyens. L'enregistrement doit être validé par 3 sages.
+Les entreprise embauche des citoyens et verse un salaire/mois de minimum 50 CTZ.
+
+## **Décès**
+
+Lorsqu'un citoyen meurt, c'est fond sont transferé dans la caisse d'impots communes par un sage.
